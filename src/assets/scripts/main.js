e48e1c1b5da37f7c1e1b117f319556d3b2468106
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-04-24 00:28:11 +0200

    Se añaden nuevos componentes

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,6 +4,7 @@
  */
 
 import * as bootstrap from 'bootstrap';
+import { Carousel } from 'bootstrap';
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
@@ -15,8 +16,23 @@ import * as bootstrap from 'bootstrap';
 document.addEventListener('DOMContentLoaded', function() {
   // Inicializar el carrusel
   var myCarousel = new Carousel(document.getElementById('carouselExampleInterval'), {
-      interval: 2000,
+      interval: 5000,
       wrap: true
   });
 });
 
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('¡Enhorabuena! Te has inscrito con éxito.', 'success')
+  })
+}
