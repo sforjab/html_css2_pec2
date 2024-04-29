635d299575c54d84da43280586de83fc07d7a08e
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-04-30 01:43:41 +0200

    Se añade styleling a los scripts. Se añaden comentarios al JS. Se realizan modificaciones de clases en las páginas y reorganización de estilos para cumplir las reglas establecidas en Stylelint

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,11 +14,12 @@ import 'animate.css';
  * Write any other JavaScript below
  */
 
+// Esperamos a que el DOM esté completamente cargado antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', function() {
-  // Verificar si el elemento del carrusel está presente en la página actual
+  // Se comprueba si el elemento del carrusel está presente en la página actual
   var carouselElement = document.getElementById('carouselInterval');
   if (carouselElement) {
-    // Si el elemento del carrusel está presente, inicializar el carrusel
+    // Si el elemento del carrusel está presente, se inicializa el carrusel
     var myCarousel = new Carousel(carouselElement, {
       interval: 5000,
       wrap: true
@@ -26,9 +27,11 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+// Obtenemos los elementos del DOM necesarios para mostrar alertas
 var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 var alertTrigger = document.getElementById('liveAlertBtn')
 
+// Función para mostrar una alerta con un mensaje y un tipo específicos
 function alert(message, type) {
   var wrapper = document.createElement('div')
   wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="submit" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
@@ -36,7 +39,9 @@ function alert(message, type) {
   alertPlaceholder.append(wrapper)
 }
 
+// Se comprueba si el botón de activación de la alerta está presente en la página actual
 if (alertTrigger) {
+  // Si el botón está presente, agrega un 'Event Listener' para mostrar la alerta al hacer clic
   alertTrigger.addEventListener('click', function () {
     alert('¡Enhorabuena! Te has inscrito con éxito.', 'success')
   })
